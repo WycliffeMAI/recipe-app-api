8348b8f50cda75ee1136f182474a5a2e33c19f6e
Wycliffe <odundoklife1@gmail.com>
2023-02-11 22:48:16 +0300

    Implemented manage yser API

--- app/user/views.py
+++ app/user/views.py
@@ -1,32 +1,30 @@
 """
-Views for the user API
+Views for the user API.
 """
+from rest_framework import generics
 from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
 from user.serializers import (
     UserSerializer,
-    AuthTokenSerializer
+    AuthTokenSerializer,
 )
-
 class CreateUserView(generics.CreateAPIView):
-    """Create as new user in the system."""
+    """Create a new user in the system."""
     serializer_class = UserSerializer
-
-
 class CreateTokenView(ObtainAuthToken):
-    """create new auth tioken for user."""
+    """Create a new auth token for user."""
     serializer_class = AuthTokenSerializer
-    render_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
-    """manage the authentication user."""
+    """Manage the authenticated user."""
     serializer_class = UserSerializer
-    authentication_class = [authentication.TokenAuthentication]
+    authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
-        """retrieve and return the authenticated user"""
+        """Retrieve and return the authenticated user."""
         return self.request.user
