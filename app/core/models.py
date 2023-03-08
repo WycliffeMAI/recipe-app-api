535d59e081fcab09d26a75461838add807c6a8b5
Wycliffe <odundoklife1@gmail.com>
2023-03-08 19:37:44 +0300

    added tGS MODEL

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,6 @@
 """
-database models
+Database models.
 """
-
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
@@ -9,49 +8,55 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
-
-
 class UserManager(BaseUserManager):
-    """manager for users"""
-
+    """Manager for users."""
     def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
         if not email:
-            raise ValueError("user must have an email address. ")
+            raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using = self._db)
+        user.save(using=self._db)
         return user
-
     def create_superuser(self, email, password):
-        """Create and return new superuser"""
+        """Create and return a new superuser."""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-
         return user
-
-
 class User(AbstractBaseUser, PermissionsMixin):
-    """User in the system"""
+    """User in the system."""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
-
+    is_staff = models.BooleanField(default=False)
     objects = UserManager()
-
-    USERNAME_FIELD = "email"
-
-
+    USERNAME_FIELD = 'email'
 class Recipe(models.Model):
-    """Recipe objects."""
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    """Recipe object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    """Tag for filtering recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
