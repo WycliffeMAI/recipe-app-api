ba054417b9a3e49400d1f5151d6fbf6fb79100b3
Wycliffe <odundoklife1@gmail.com>
2023-03-08 21:20:32 +0300

    added test and implemented updating recipe api tags

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -11,6 +11,7 @@ from core.models import (
 class TagSerializer(serializers.ModelSerializer):
     """Serializer for tags."""
 
+
     class Meta:
         model = Tag
         fields = ['id', 'name']
@@ -20,6 +21,7 @@ class TagSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
     tags = TagSerializer(many=True, required=False)
+
 
     class Meta:
         model = Recipe
@@ -32,10 +34,9 @@ class RecipeDetailSerializer(RecipeSerializer):
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
 
-    def create(self, validated_data):
-        """Create a recipe."""
-        tags = validated_data.pop('tags', [])
-        recipe = Recipe.objects.create(**validated_data)
+
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed."""
         auth_user = self.context['request'].user
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(
@@ -44,4 +45,24 @@ class RecipeDetailSerializer(RecipeSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def create(self, validated_data):
+        """Create a recipe."""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context['request'].user
+        self._get_or_create_tags(tags, recipe)
+
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update recipe."""
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
