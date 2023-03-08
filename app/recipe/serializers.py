d24d3e799aeeffbc18c140773098b1cc120bd509
Wycliffe <odundoklife1@gmail.com>
2023-03-08 20:12:58 +0300

    added tags api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -2,17 +2,31 @@
 Serializers for recipe APIs
 """
 from rest_framework import serializers
+
 from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
-
-
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
